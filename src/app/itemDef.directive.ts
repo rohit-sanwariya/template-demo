fca7970e8fa06d50d1a299eaf1f55f2f942715b3
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2025-08-01 11:15:06 +0530

    type working

--- src/app/itemDef.directive.ts
+++ src/app/itemDef.directive.ts
@@ -1,11 +1,19 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Directive, inject, input, TemplateRef } from '@angular/core';
+
+type ItemContext<T> = {
+  $implicit: T;
+}
+
 
 @Directive({
   selector: '[appItemDef]'
 })
 export class ItemDefDirective<T> {
-  readonly templateRef = inject(TemplateRef<T>)
-  readonly data = input<T[]>();
-  constructor() { }
+  readonly templateRef = inject(TemplateRef<ItemContext<T>>)
+  readonly appItemDefData = input<T[]>();
+  static ngTemplateContextGuard<T>  (dir:ItemDefDirective<T>,context: unknown): context is ItemContext<T> {
+    return true;
+  }
 
 }
